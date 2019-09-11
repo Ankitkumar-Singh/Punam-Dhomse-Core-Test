b4465d36ac07c4b5ef5eb257a2ca4adac442869c
Punam Dhomse <punam.dhomsee@aressindia.net>
2019-09-11 18:30:06 +0530

    update

---
+++ ProductManagement/ProductManagement/Program.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+
+namespace ProductManagement
+{
+    public class Program
+    {
+        #region "Main"
+        //Defines the entry point of the application.
+        //<param name="args">The arguments.</param>
+        public static void Main(string[] args) => CreateWebHostBuilder(args).Build().Run();
+        #endregion
+
+        #region "CreateWebHostBuilder"
+        //Creates the web host builder.
+        //<param name="args">The arguments.</param>
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>();
+        #endregion
+    }
+}
